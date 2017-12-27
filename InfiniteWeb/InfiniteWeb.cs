bcf6351c5073610aa2756f65fdb2020cbf9f26fd
chaitradangat084 <chaitradangat084@outlook.com>
2017-12-28 03:28:04 +0530

    A initial Template to get started!

---
+++ InfiniteWeb/InfiniteWeb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteWeb
+{
+    public class WebQuery
+    {
+        public static List<string> urls_ { get; set; }
+
+        public string Scraped_Text { get; set; }
+
+        public string Scraped_Urls { get; set; }
+
+        public void GetPage(string url)
+        {
+
+        }
+
+        public void GetText(string page)
+        {
+
+        }
+
+        public void GetUrls(string page)
+        {
+            
+        }
+    }
+}
